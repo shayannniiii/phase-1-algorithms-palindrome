8054dda71fc5f088e87798554993d874f6b52fb8
ali taslimpoor <shayannniiii@gmail.com>
2023-04-25 14:30:36 +0300

    completed

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function isPalindrome(str) {
+  // Convert the string to lowercase
+  str = str.toLowerCase();
+
+  for (let i = 0; i < str.length / 2; i++) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 }
+
 
 /* 
   Add your pseudocode here
